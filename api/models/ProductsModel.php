bff638fef3b06e1b0ea6f270d0279e8b9807ccad
Will <williansilva3dsi@hotmail.com>
2023-02-23 18:55:36 -0300

    refactor: rewriting query string to match products data

--- api/models/ProductsModel.php
+++ api/models/ProductsModel.php
@@ -2,17 +2,13 @@
 
   class ProductModel{
     private $db;
-    private $table = 'products';
-    // public $id;
-    // public $name;
-    // public $price;
 
     public function __construct($db) {
       $this->db = $db;
     }
 
     public function getAll() {
-      $query = 'SELECT * FROM ' . $this->table;
+      $query = "SELECT P.sku, P.name, P.price,C.category_name, P.attr_value FROM products AS P INNER JOIN categories AS C;";
       $stmt = $this->db->prepare($query);
       $stmt->execute();
       return $stmt;
