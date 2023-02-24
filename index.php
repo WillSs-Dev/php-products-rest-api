4342c30bc4633dc1dc4cc1f9db43133372e32fe8
Will <williansilva3dsi@hotmail.com>
2023-02-24 16:30:09 -0300

    feat: autoload class associates api endpoints to their controllers classes

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
 
-// pre-loads all classes
-
-spl_autoload_register(function ($class_name) {
-    include 'src/' . $class_name . '.php';
-});
-
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Content-Type: application/json');
 
-new ProductsController();
+class AutoLoad
+{
+
+  private $endpoint;
+
+  public function __construct()
+  {
+    spl_autoload_register(function ($class_name) {
+      include 'src/' . $class_name . '.php';
+    });
+
+    $this->endpoint = $_SERVER["REQUEST_URI"];
+    $this->getController();
+  }
+
+  public function getController()
+  {
+    switch ($this->endpoint) {
+      case '/products':
+        new ProductsController();
+        break;
+    }
+  }
+}
+
+new AutoLoad();
