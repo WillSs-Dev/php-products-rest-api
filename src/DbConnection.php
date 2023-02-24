11028b3838f317c74bea3aa1516eed830cb89e5b
Will <williansilva3dsi@hotmail.com>
2023-02-24 17:31:23 -0300

    refactor: changes env names to match remote mysql specifications

--- src/DbConnection.php
+++ src/DbConnection.php
@@ -4,19 +4,23 @@ class DbConnection
   private $host;
   private $user;
   private $pass;
+  private $db_name;
+  private $port;
   private $db = null;
 
   public function __construct()
   {
-    $this->host = getenv('DB_HOST');
-    $this->user = getenv('DB_USER');
-    $this->pass = getenv('DB_PASS');
+    $this->host = getenv('MYSQLHOST');
+    $this->user = getenv('MYSQLUSER');
+    $this->pass = getenv('MYSQLPASSWORD');
+    $this->db_name = getenv('MYSQLDATABASE');
+    $this->port = getenv('MYSQLPORT');
   }
 
   public function connect()
   {
     try {
-      $this->db = new PDO("mysql:host=$this->host;dbname=store", $this->user, $this->pass);
+      $this->db = new PDO("mysql:host=$this->host;dbname=$this->db_name;port=$this->port", $this->user, $this->pass);
       $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
       echo "Connection Error: " . $e->getMessage();
