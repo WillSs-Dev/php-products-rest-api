360286f80ffac724935e98959ed3495c9a7f7e93
Willian <williansilva3dsi@hotmail.com>
2023-03-02 00:05:48 -0300

    refactor: adds more headers to solve problem with cors

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: *');
-header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+header("Access-Control-Max-Age", "3600");
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header("Access-Control-Allow-Credentials", "true");
 
 class AutoLoad
 {
