1038ab2910a916d2e8ff51765c93f85c2905e98d
Willian <williansilva3dsi@hotmail.com>
2023-03-02 21:51:46 -0300

    refactor: stores furniture attr_value inside the $dimensions property

--- src/domain/Furniture.php
+++ src/domain/Furniture.php
@@ -2,9 +2,7 @@
 
 class Furniture extends Product
 {
-  public $height;
-  public $width;
-  public $length;
+  public $dimensions;
 
   public function __construct($sku, $name, $price, $category, $attr_value)
   {
@@ -14,9 +12,6 @@ class Furniture extends Product
 
   public function set_attr_value($attr_value)
   {
-    $attr_value = explode('x', $attr_value);
-    $this->height = $attr_value[0] . 'm';
-    $this->width = $attr_value[1] . 'm';
-    $this->length = $attr_value[2] . 'm';
+    $this->dimensions = $attr_value;
   }
 }
