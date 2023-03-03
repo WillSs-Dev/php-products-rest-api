376c45b24aedd39c44ab5af66c8e33ab57515abe
Willian <williansilva3dsi@hotmail.com>
2023-03-02 22:41:54 -0300

    removing credentials header

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header("Access-Control-Allow-Credentials", "true");
 header('Content-Type: application/json');
 
 class AutoLoad
